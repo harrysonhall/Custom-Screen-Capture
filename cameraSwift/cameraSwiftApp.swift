3516fd280331f3e1e5823292adc0921737547404
harrysonhall <hhall@sitemetric.com>
2024-05-26 23:03:23 -0400

    Initial Commit

---
+++ cameraSwift/cameraSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  cameraSwiftApp.swift
+//  cameraSwift
+//
+//  Created by Harrison Hall on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct cameraSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
